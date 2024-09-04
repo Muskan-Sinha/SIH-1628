2cc78d43680bcc9722bee5621a370c73a47e8209
mr-Dev-Anshu <anshuies31@gmail.com>
2024-09-05 00:42:19 +0530

    update changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,38 @@ import { FaRegUser } from "react-icons/fa";
 import Sidebar from "./Sidebar";
 
 const Navbar = () => {
-    return (
-        <div className="">
-            <div className="flex justify-between bg-white">
-                <p className="p-3 ml-4 sm:ml-10 text-2xl font-semibold">CareerAlly</p>
-                <div className="flex">
-                    <p className="p-3 text-2xl md:mr-16 hover:cursor-pointer"><FaRegUser /></p>
-                    <p className="sm:hidden right-0 "><Sidebar /></p>
-                </div>
-            </div>
-            <hr />
-            <div className="flex justify-around text-white bg-[#688a84] ">
-                <p className="p-3 hover:bg-[#EAFFFC] rounded-md hover:px-6 hover:cursor-pointer">Training</p>
-                <p className="p-3 hidden sm:flex hover:bg-[#EAFFFC] rounded-md hover:px-6 hover:cursor-pointer">Skill Verification</p>
-                <p className="p-3 hover:bg-[#EAFFFC] rounded-md hover:px-6 hover:cursor-pointer">Resume Builder</p>
-                <p className="p-3 hover:bg-[#EAFFFC] rounded-md hover:px-6 hover:cursor-pointer">Job Recommendation</p>
-            </div>
+  return (
+    <div className="">
+      <div className="flex justify-between bg-white items-center">
+        <p className="px-3 py-6  ml-4 sm:ml-10 text-2xl font-semibold">
+          CareerAlly
+        </p>
+        <div className="flex">
+          <p className="p-3 text-2xl md:mr-16 hover:cursor-pointer">
+            <FaRegUser />
+          </p>
+          <p className="sm:hidden right-0 ">
+            <Sidebar />
+          </p>
         </div>
-    )
-}
+      </div>
+      <hr />
+      <div className="flex justify-around   text-white bg-[#4b7a73] ">
+        <p className="p-3 transition-all  text-sm font-semibold   hover:scale-110 rounded-md  hover:cursor-pointer">
+          Training
+        </p>
+        <p className="p-3 hidden sm:flex text-sm font-semibold  transition-all hover:scale-110 rounded-md hover:cursor-pointer">
+          Skill Verification
+        </p>
+        <p className="p-3 transition-all text-sm font-semibold  hover:scale-110 rounded-md hover:cursor-pointer">
+          Resume Builder
+        </p>
+        <p className="p-3 transition-all text-sm font-semibold  hover:scale-110 rounded-md hover:cursor-pointer ">
+          Job Recommendation
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
